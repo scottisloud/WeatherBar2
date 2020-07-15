14794339e81da7fb6912dc015d84c4d3e0273b49
Scott Lougheed <scott@scottlougheed.com>
2020-07-15 15:11:00 -0700

    Initial Commit

---
+++ WeatherBar2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherBar2
+//
+//  Created by Scott Lougheed on 2020/07/15.
+//  Copyright © 2020 Scott Lougheed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
